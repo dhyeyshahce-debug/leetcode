742be9a80b9633efde2e18706941d80276a0a755
dhyeyshahce-debug <dhyeyshahce@gmail.com>
2026-07-12 23:35:10 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/12/2026, 11:35:10 PM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int x:nums){
+            mp[x]++;
+            if(mp[x]>nums.size()/2){
+                return x;
+            }
+        }
+         return -1;
+    }
+};
