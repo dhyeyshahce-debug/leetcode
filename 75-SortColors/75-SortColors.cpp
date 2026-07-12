19d44dd811f6b009051be2eca39fe36fc241f998
dhyeyshahce-debug <dhyeyshahce@gmail.com>
2026-07-12 23:35:20 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/12/2026, 11:35:20 PM
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int count0=0,count1=0,count2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+            {
+                count0++;
+            }
+            else if(nums[i]==1)
+            {
+                count1++;
+            }
+            else
+            {
+                count2++;
+            }
+        }
+        int i=0;
+        while(count0--)nums[i++]=0;
+        while(count1--)nums[i++]=1;
+        while(count2--)nums[i++]=2;
+    }
+};
