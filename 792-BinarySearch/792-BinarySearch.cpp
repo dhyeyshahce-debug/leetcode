8d7275434d85ad30c5cbf98bc3dbfb5a5472befa
dhyeyshahce-debug <dhyeyshahce@gmail.com>
2026-07-12 23:34:31 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/12/2026, 11:34:32 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return -1; 
+    }
+};
