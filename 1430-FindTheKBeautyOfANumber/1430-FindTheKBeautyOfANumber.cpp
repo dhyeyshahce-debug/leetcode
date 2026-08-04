d68b524e8c583cbe6f4fd72dd3018c5f9ae479b7
dhyeyshahce-debug <dhyeyshahce@gmail.com>
2026-08-04 23:18:49 +0530

    Create 1430-FindTheKBeautyOfANumber

---
+++ 1430-FindTheKBeautyOfANumber/1430-FindTheKBeautyOfANumber.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/4/2026, 11:18:48 PM
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        string s=to_string(num);
+        int count=0;
+
+        for(int i=0;i<=s.size()-k;i++)
+        {
+            int val=stoi(s.substr(i,k));
+            if(val!=0 && num%val==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
