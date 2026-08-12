e8cde60169403eef8bf72b7dbe608f27a12c23bc
dhyeyshahce-debug <dhyeyshahce@gmail.com>
2026-08-12 22:22:11 +0530

    Create 904-FruitIntoBaskets

---
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/12/2026, 10:22:11 PM
+1class Solution {
+2public:
+3    int search(vector<int>& nums, int target) {
+4        int n=nums.size();
+5        int low=0;
+6        int high=n-1;
+7        while(low<=high)
+8        {
+9            int mid=low+(high-low)/2;
+10            if(nums[mid]==target)return mid;
+11            if(nums[mid]<target)low=mid+1;
+12            else high=mid-1;
+13        }
+14        return -1;
+15    }
+16};
