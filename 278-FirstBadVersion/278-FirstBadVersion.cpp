f4c15efab4e18ac1a008cb6a468a272b1dc157cd
dhyeyshahce-debug <dhyeyshahce@gmail.com>
2026-08-15 23:19:47 +0530

    File updated at 8/15/2026, 11:19:47 PM

--- 278-FirstBadVersion/278-FirstBadVersion.cpp
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -1,24 +1,25 @@
-// Last updated: 8/15/2026, 11:06:56 PM
-1// The API isBadVersion is defined for you.
-2// bool isBadVersion(int version);
-3
-4class Solution {
-5public:
-6    int firstBadVersion(int n) {
-7        int low=1;
-8        int high=n;
-9        while(low<high)
-10        {
-11            int mid=low+(high-low)/2;
-12            if(isBadVersion(mid))
-13            {
-14                high=mid;
-15            }
-16            else
-17            {
-18                low=mid+1;
-19            }
-20        }
-21        return low;
-22    }
-23};
+// Last updated: 8/15/2026, 11:19:47 PM
+1/** 
+2 * Forward declaration of guess API.
+3 * @param  num   your guess
+4 * @return 	     -1 if num is higher than the picked number
+5 *			      1 if num is lower than the picked number
+6 *               otherwise return 0
+7 * int guess(int num);
+8 */
+9
+10class Solution {
+11public:
+12    int guessNumber(int n) {
+13        int low=1;
+14        int high=n;
+15        while(low<=high)
+16        {
+17            int mid=low+(high-low)/2;
+18            if(guess(mid)==0)return mid;
+19            else if(guess(mid)==-1)high=mid-1;
+20            else low=mid+1;
+21        } 
+22        return -1;
+23    }
+24};
