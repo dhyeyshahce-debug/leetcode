6639fa34ae1f3f31e3336db8564a0d172671340d
dhyeyshahce-debug <dhyeyshahce@gmail.com>
2026-08-20 23:31:01 +0530

    File updated at 8/20/2026, 11:31:00 PM

--- 15-3Sum/15-3Sum.cpp
+++ 15-3Sum/15-3Sum.cpp
@@ -1,27 +1,26 @@
-// Last updated: 8/20/2026, 11:04:15 PM
+// Last updated: 8/20/2026, 11:31:00 PM
 1class Solution {
 2public:
-3    vector<vector<int>> threeSum(vector<int>& nums) {
-4        int n=nums.size();
-5        set<vector<int>>st;
-6        sort(nums.begin(),nums.end());
-7        for(int i=0;i<n-2;i++)
-8        {
-9            int l=i+1;
-10            int r=n-1;
-11            while(l<r){
-12                int sum = nums[i] + nums[l] + nums[r];
-13                if(sum==0)
-14                {
-15                    st.insert({nums[i],nums[l],nums[r]});
-16                    l++;
-17                    r--;
-18                }
-19                else if(sum>0)r--;
-20                else l++;
-21            } 
+3    int trap(vector<int>& height) {
+4        int n=height.size();
+5        vector<int>prefix(n);
+6        vector<int>suffix(n);
+7        int ans=0;
+8        prefix[0]=height[0];
+9        for(int i=1;i<n;i++)
+10        {
+11            prefix[i]=max(prefix[i-1],height[i]);
+12        }
+13        suffix[n-1]=height[n-1];
+14        for(int i=n-2;i>=0;i--)
+15        {
+16            suffix[i]=max(suffix[i+1],height[i]);
+17        }
+18        for(int i=0;i<n;i++)
+19        {
+20            int min_water=min(prefix[i],suffix[i]);
+21            ans+=min_water-height[i];
 22        }
-23        vector<vector<int>> ans(st.begin(),st.end());
-24        return ans;
-25    }
-26};
+23     return ans;
+24    }
+25};
